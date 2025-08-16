2ee36a6227e856d9eb28657be1d332d0e0b7ee32
gvnrds Windows Desktop <gvnrx@users.noreply.github.com>
2025-08-16 12:40:04 -0400

    Edited vite.config settings

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Jammming/",
   plugins: [react()],
   server: {
-// Spotify wouldn't accept localhost URI
+    // Spotify wouldn't accept localhost URI
     host: "127.0.0.1",
     port: 5173,
   },
